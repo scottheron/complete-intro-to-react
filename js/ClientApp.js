ce7505ae27c0ca0d64089ffc9a0026f203d761a0
scottheron <scott.heron@outlook.com>
2017-03-28 10:42:37 -0700

    2

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,10 +1,12 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-  render: function(){
+  render: function () {
     return (
-      div(null, 
+      div(null,
         h1({style: {color: this.props.color, fontweight: 'bold'}}, this.props.title)
       )
     )
@@ -14,7 +16,7 @@ var MyTitle = React.createClass({
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
-  render: function(){
+  render: function () {
     return (
       div(null,
         MyTitleFactory({title: 'props are the best', color: 'salmon'}),
@@ -25,4 +27,5 @@ var MyFirstComponent = React.createClass({
     )
   }
 })
+
 ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
